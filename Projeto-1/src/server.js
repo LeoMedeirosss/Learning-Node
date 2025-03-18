81e211a076ee9ef41089d8839e3d66bdb5ec2d3c
LeoMedeirosss <lmf@cesar.school>
2025-03-18 17:36:24 -0300

    save users in memory 1.0

--- Projeto-1/src/server.js
+++ Projeto-1/src/server.js
@@ -1,7 +1,26 @@
 import http from "node:http"
 
+const users = []
+
 const server = http.createServer((req, res) => {
-    return res.end("first alo")
+    const { method, url} = req
+
+    if (method === "GET" && url === "/users") {
+
+        return res.end(users)
+    }
+
+    if (method === "POST" && url === "/users") {
+        users.push({
+            id: 1,
+            name: "alguem",
+            email:"alguem@email.com",
+        })
+
+        return res.end("criação de usuário")
+    }   
+
+    return res.end("first test")
 })
 
 server.listen(3000)
